07e3fe7c37cae4b2d33a68c66f73e8b4f4b13eba
Andrey Helldar <helldar@ai-rus.com>
2022-02-28 11:42:09 +0300

    Improved handling of null values

--- src/Services/Stringify.php
+++ src/Services/Stringify.php
@@ -1,46 +1,1 @@
-<?php
-
-namespace DragonCode\EnvSync\Services;
-
-use DragonCode\Support\Concerns\Makeable;
-use DragonCode\Support\Facades\Helpers\Boolean;
-
-class Stringify
-{
-    use Makeable;
-
-    public function get($value): string
-    {
-        switch (true) {
-            case $this->isBool($value):
-                return $this->fromBool($value);
-
-            case $this->isNumeric($value):
-            case $this->isString($value):
-                return $value;
-
-            default:
-                return '';
-        }
-    }
-
-    public function isBool($value): bool
-    {
-        return is_bool($value);
-    }
-
-    public function isNumeric($value): bool
-    {
-        return is_numeric($value);
-    }
-
-    public function isString($value): bool
-    {
-        return is_string($value) && ! empty($value);
-    }
-
-    public function fromBool(bool $value): string
-    {
-        return Boolean::convertToString($value);
-    }
-}
+<?phpnamespace DragonCode\EnvSync\Services;use DragonCode\Support\Concerns\Makeable;use DragonCode\Support\Facades\Helpers\Boolean;use DragonCode\Support\Facades\Helpers\Str;class Stringify{    use Makeable;    public function get($value): string    {        switch (true) {            case $this->isBool($value):                return $this->fromBool($value);            case $this->isNull($value):                return $this->fromNull();            case $this->isNumeric($value):            case $this->isString($value):                return $value;            default:                return '';        }    }    public function isBool($value): bool    {        return is_bool($value);    }    public function isNumeric($value): bool    {        return is_numeric($value);    }    public function isString($value): bool    {        return is_string($value) && ! empty($value);    }    public function isNull($value): bool    {        return is_null($value) || Str::lower($value) === 'null';    }    public function fromBool(bool $value): string    {        return Boolean::convertToString($value);    }    public function fromNull(): string    {        return '';    }}
